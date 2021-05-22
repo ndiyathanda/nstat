12cd96606fec898254d63e684b16988e6d6bf8eb
ndiyathanda <77878599+ndiyathanda@users.noreply.github.com>
2021-05-22 21:39:42 +0200

    Update nstat.sh

--- nstat.sh
+++ nstat.sh
@@ -1,15 +1,17 @@
 while :
 do
 echo "System kernel:";uname -srm 
-echo "Uptime:";uptime
-echo CPU info:
+echo -e "Uptime:";uptime
+echo -e CPU info:
 sudo dmidecode -t 4 | egrep -i 'core (count|enabled)|thread count|Version'
-echo CPU usage;grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
-echo GPU info [only nvidia cards works!];
+echo -e CPU usage;grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}'
+echo -e GPU info [only nvidia cards works!];
 nvidia-smi -L
-echo GPU Usage:
+echo -e GPU Usage:
 nvidia-smi --format=csv --query-gpu=power.draw,utilization.gpu,fan.speed,temperature.gpu
-echo Memory usage; free -t | awk 'NR == 2 {print $3/$2*100"%"}'
-sleep 2
+echo -e Memory usage; free -t | awk 'NR == 2 {print $3/$2*100"%"}'
+echo -e Average ping
+ping -c 4 google.com | tail -1| awk '{print $4}' | cut -d '/' -f 2
+sleep 1
 clear
 done
